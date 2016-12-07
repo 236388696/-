f63331c342f24017d7fd7a76119b6975b08c82f2
panpanzhuren <236388696@qq.com>
2016-12-07 11:42:48 +0800

    xiecheng3

--- 0811js初级/react/Demo/xiecheng.js
+++ 0811js初级/react/Demo/xiecheng.js
@@ -14,30 +14,33 @@ class Xiecheng extends Component {
     render() {
         return (
             <View style={XiechengStyle.container}>
-                <View style={XiechengStyle.item}>
+                <View>
                     <TextInput style={{width:380,height:40,borderWidth:1,borderColor:'#ECEDF1'}} placeholder={'搜索：目的地／酒店／景点／航班号'} />
                 </View>
                 <View style={XiechengStyle.item}>
                     <Image source={require("./2.png")}/>
                 </View>
-                <View style={[XiechengStyle.item, XiechengStyle.center]}>
-                    <Text>酒店</Text>
-                </View>
-                <View style={XiechengStyle.item}>
-                    <View style={[XiechengStyle.innerView, XiechengStyle.center]}>
-                        <Text>海外酒店</Text>
+                <View style={[XiechengStyle.item, XiechengStyle.center,XiechengStyle.count3,XiechengStyle.row1]}>
+                    <View style={XiechengStyle.item1}>
+                        <Text>酒店</Text>
+                    </View>
+                    <View style={XiechengStyle.item}>
+                    <View style={[XiechengStyle.innerView, XiechengStyle.center,XiechengStyle.item2]}>
+                        <Text style={XiechengStyle.margin1}>海外酒店</Text>
                     </View>
                     <View style={[XiechengStyle.innerView, XiechengStyle.center]}>
                         <Text>特价酒店</Text>
                     </View>
-                </View>
-                <View style={XiechengStyle.item}>
-                    <View style={[XiechengStyle.innerView, XiechengStyle.center]}>
-                        <Text>团购</Text>
                     </View>
-                    <View style={[XiechengStyle.innerView, XiechengStyle.center]}>
-                        <Text>客栈公寓</Text>
+                    <View style={XiechengStyle.item}>
+                        <View style={[XiechengStyle.innerView, XiechengStyle.center]}>
+                            <Text>团购</Text>
+                        </View>
+                        <View style={[XiechengStyle.innerView, XiechengStyle.center]}>
+                            <Text>客栈公寓</Text>
+                        </View>
                     </View>
+
                 </View>
             </View>
         )
@@ -48,13 +51,13 @@ class Xiecheng extends Component {
 const XiechengStyle = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: "row",
+        flexDirection: "column",
         marginTop: 20,
-        backgroundColor: '#ccc'
+        backgroundColor: '#F6F6F6'
     },
     item: {
-        flex: 1,
-        //height: 80,
+        // flex: 1,
+        height: 60
 
     },
     innerView: {
@@ -63,6 +66,26 @@ const XiechengStyle = StyleSheet.create({
     center: {
         justifyContent: "center",
         alignItems: 'center'
+    },
+    row1:{
+        flexDirection: "row"
+    },
+    count3:{
+        backgroundColor:"#FF697A",
+        marginTop:20
+
+    },
+    margin1:{
+        marginRight:5,
+        marginLeft:10
+    },
+    item1:{
+        width:100,
+        marginRight:2
+    },
+    item2:{
+        width:100,
+        margin:2
     }
 
 });
